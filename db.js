7a953291d1ffe354c6a971bc5133a66f28d30c1a
RUPESH KUMBHAR <rupesh.p.kumbhar@gmail.com>
2024-08-24 15:23:18 +0530

    backend first commit

---
+++ db.js
@@ -0,0 +1,26 @@
+// const mongoose = require('mongoose');
+
+// mongoose.connect('mongodb://localhost:27017/meanDB', (err)=>{
+//     if(!err){
+//         console.log('DB Connnection Successful')
+//     }
+//     else{
+//         console.log('Error in Connnection ' + err)
+
+//     }
+// })
+
+// module.exports = mongoose;
+//////////////////////////////////////////////////
+const mongoose = require("mongoose");
+
+const mongoURI = "mongodb://localhost:27017/meanDB";
+
+mongoose
+  .connect(mongoURI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB: " + err.message);
+  });
