7f3b71e246c6884c04c29d25b86c4391a5619700
RUPESH KUMBHAR <rupesh.p.kumbhar@gmail.com>
2024-08-25 10:54:38 +0530

    GET by ID, GET ALL, PUT Api

--- routes/routes.js
+++ routes/routes.js
@@ -31,6 +31,7 @@
 const express = require('express');
 const router = express.Router();
 const Employee = require('../models/employee.js');
+const ObjectId = require('mongoose').Types.ObjectId;
 
 // POST request to add a new employee
 router.post('/employee', async (req, res) => {
@@ -48,5 +49,90 @@ router.post('/employee', async (req, res) => {
         res.status(500).send(err);
     }
 });
+
+
+// GET request to add a new employee
+
+router.get('/list', (req, res) => {
+    Employee.find().exec()
+        .then(docs => {
+            res.send(docs);
+        })
+        .catch(err => {
+            console.log('Error in retrieving data: ' + err);
+            res.status(500).send({ error: 'Failed to retrieve employee data', details: err });
+        });
+});
+
+
+//get by id
+router.get('/:id', (req, res) => {
+    if (ObjectId.isValid(req.params.id)) {
+        Employee.findById(req.params.id).exec()
+            .then(doc => {
+                if (doc) {
+                    res.send(doc);
+                } else {
+                    res.status(404).send('No record found with ID ' + req.params.id);
+                }
+            })
+            .catch(err => {
+                console.log('Error in Get Employee by ID: ' + err);
+                res.status(500).send({ error: 'Failed to retrieve employee data', details: err });
+            });
+    } else {
+        return res.status(400).send('Invalid ID format: ' + req.params.id);
+    }
+});
+
+
+//Delete by id
+router.delete('/:id', (req, res) => {
+    if (ObjectId.isValid(req.params.id)) {
+        Employee.findByIdAndDelete(req.params.id).exec()
+            .then(doc => {
+                if (doc) {
+                    res.send(doc);
+                } else {
+                    res.status(404).send('No record found with ID ' + req.params.id);
+                }
+            })
+            .catch(err => {
+                console.log('Error in Delete Employee by ID: ' + err);
+                res.status(500).send({ error: 'Failed to retrieve employee data', details: err });
+            });
+    } else {
+        return res.status(400).send('Invalid ID format: ' + req.params.id);
+    }
+});
+
+
+//PUT Api
+
+router.put('/:id', (req, res) => {
+    let emp = {
+        name: req.body.name,
+        position: req.body.position,
+        dept: req.body.dept
+    };
+
+    if (ObjectId.isValid(req.params.id)) {
+        Employee.findByIdAndUpdate(req.params.id, { $set: emp }, { new: true })
+            .then(doc => {
+                if (!doc) {
+                    return res.status(404).send('No record found with ' + req.params.id);
+                }
+                res.send(doc);
+            })
+            .catch(err => {
+                console.log('Error in Update Employee by id: ' + err);
+                res.status(500).send('Internal Server Error');
+            });
+    } else {
+        return res.status(400).send('No record found with ' + req.params.id);
+    }
+});
+
+
 
 module.exports = router;
